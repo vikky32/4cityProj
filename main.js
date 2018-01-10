406ff64e75e5d0c5febc26e79d089c8160685d49
Vika <vika97777@gmail.com>
2018-01-10 14:06:12 +0200

    "new commit"

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var radioBtns = document.querySelectorAll(".radio-btn");
 var uploadFile = document.querySelector("#file");
 var phoneNumber = document.querySelector("#phone-number");
 var chosenFile = document.querySelector("#show-file");
-
+var showIdeas = document.querySelector(".show-more");
+var moreNews = document.querySelector(".container-block-left-core-more");
 
 //запрет на ввод буквенных символов
 
@@ -22,24 +23,27 @@ function showFile() {
     chosenFile.style.display = "inline-block";
 }
 
-
-//очистка формы
-
 resetAll.addEventListener("click", function () {
-    for(var resText = 0; resText < inputItems.length; resText++){
-        inputItems[resText].value = "";
-    }
+    chosenFile.style.display = "none";
 
-    for(var resBtn =0; resBtn < radioBtns.length; resBtn++ ){
-        radioBtns[resBtn].checked = false;
-        this.blur();
-    }
-    phoneNumber.value = "+38";
 });
-
-var showIdeas = document.querySelector(".show-more");
-var moreNews = document.querySelector(".container-block-left-core-more");
 
 showIdeas.addEventListener("click", function () {
     moreNews.style.display = "block";
 });
+//очистка формы
+// resetAll.addEventListener("click", function () {
+//     for(var resText = 0; resText < inputItems.length; resText++){
+//         inputItems[resText].value = "";
+//     }
+//
+//     for(var resBtn =0; resBtn < radioBtns.length; resBtn++ ){
+//         radioBtns[resBtn].checked = false;
+//         this.blur();
+//     }
+//     phoneNumber.value = "+38";
+//
+//     clearFile()
+//
+// });
+
